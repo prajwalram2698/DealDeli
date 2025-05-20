77f3a5efb0642dfbc557c409e6df616ddd052d96
prajwalram2698 <prajwalramesh2605@gmail.com>
2025-05-20 20:31:13 +0100

    updated user microservice

--- user-microservice/app.py
+++ user-microservice/app.py
@@ -20,4 +20,6 @@ app.register_blueprint(routes)
 
 # Run the Flask application
 if __name__ == '__main__':
-    app.run(debug=True)  # You can set debug=False in production
+    app.run(host='0.0.0.0', port=5001,debug=True)
+
+    #app.run(debug=True)  # You can set debug=False in production
